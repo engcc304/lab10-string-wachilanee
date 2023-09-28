d10f695b9373939a179aa3ce40aa4a0d537449fd
wachilanee <wachilanee47@gmail.com>
2023-09-28 09:35:24 +0700

    testlab10

--- Lab/Lab10-String.cpp
+++ Lab/Lab10-String.cpp
@@ -28,10 +28,52 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+
+// เช็คว่าเป็นพาลาไหม 
+int is_palindrome(char *word) {
+    int length = strlen(word);
+    for (int i = 0; i < length / 2; i++) {
+        if (word[i] != word[length - i - 1]) {
+            return 0; 
+        }
+    }
+    return 1; 
+}
 
 int main() {
+    char user_input[100];
+    printf("Enter word: ");
+    scanf("%s", user_input);
 
-    //--| YOUR CODE HERE
+    if (is_palindrome(user_input)) {
+        printf("Pass.\n");
+    } else {
+        // ตรวจสอบว่าคำนั้นสามารถอ่านจากหน้าไปหลังหรือหลังไปหน้าได้
+        int front_to_back = 1;
+        int back_to_front = 1;
 
-    return 0 ;
-}//end main function
+        int length = strlen(user_input);
+        for (int i = 0; i < length / 2; i++) {
+            if (user_input[i] != user_input[length - i - 1]) {
+                front_to_back = 0;
+                break;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (user_input[i] != user_input[length - i - 1]) {
+                back_to_front = 0;
+                break;
+            }
+        }
+
+        if (front_to_back || back_to_front) {
+            printf("Pass.\n");
+        } else {
+            printf("Not Pass.\n");
+        }
+    }
+
+    return 0;
+}
